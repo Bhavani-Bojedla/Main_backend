26464dbefdef1063fd0ecb7fd22ac3af37c3be0a
BHAVANIBOJEDLA <bhavanibojadla8@gmail.com>
2024-09-15 14:23:39 +0530

    your message

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { fetchRouter } = require("./routes/fetch.toute");
 const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
 port = 3000;
 app.use("/", fetchRouter);
 app.listen(port, () => {
